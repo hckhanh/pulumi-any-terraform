dd2f87c3d8b014e7832c7e5aa379383e2caa14b1
Khánh Hoàng <hi@khanh.id>
2025-09-29 16:46:13 +0700

    Update bunnynet version to 0.10.1 and adjust parameterization values

--- packages/bunnynet/utilities.ts
+++ packages/bunnynet/utilities.ts
@@ -110,8 +110,8 @@ export async function getPackage() : Promise<string | undefined> {
 			const monitor = runtime.getMonitor();
 			const params = new resproto.Parameterization();
 			params.setName("bunnynet");
-			params.setVersion("0.10.0");
-			params.setValue(Uint8Array.from(atob("eyJyZW1vdGUiOnsidXJsIjoicmVnaXN0cnkub3BlbnRvZnUub3JnL2J1bm55d2F5L2J1bm55bmV0IiwidmVyc2lvbiI6IjAuMTAuMCJ9fQ=="), c => c.charCodeAt(0)));
+			params.setVersion("0.10.1");
+			params.setValue(Uint8Array.from(atob("eyJyZW1vdGUiOnsidXJsIjoicmVnaXN0cnkub3BlbnRvZnUub3JnL2J1bm55d2F5L2J1bm55bmV0IiwidmVyc2lvbiI6IjAuMTAuMSJ9fQ=="), c => c.charCodeAt(0)));
 
 			const req = new resproto.RegisterPackageRequest();
 			req.setName("terraform-provider");
