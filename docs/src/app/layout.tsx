ae0da37c6741def55572128a4319bd89629d773d
Khánh Hoàng <hi@khanh.id>
2025-11-06 19:35:38 +0700

    Add custom font setup for docs layout

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,14 +1,14 @@
 import { RootProvider } from 'fumadocs-ui/provider/next'
 import './global.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({
-  subsets: ['latin'],
-})
+import { inter, jetbrainsMono } from '@/lib/fonts'
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html className={inter.className} lang='en' suppressHydrationWarning>
+    <html
+      className={`${inter.variable} ${jetbrainsMono.variable}`}
+      lang='en'
+      suppressHydrationWarning
+    >
       <body className='flex min-h-screen flex-col'>
         <RootProvider>{children}</RootProvider>
       </body>
